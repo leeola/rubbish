d19df0bf01523da6cebf82ad4dad48bbdb3165c5
Lee Olayvar <leeolayvar@gmail.com>
2017-05-13 15:56:01 -0700

    feat: use new fixity autoload api

--- cmd/rubbish/loadconfig.go
+++ cmd/rubbish/loadconfig.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"os"
-	"path/filepath"
 
 	"github.com/BurntSushi/toml"
 	"github.com/leeola/errors"
-	"github.com/leeola/fixity/impl/local"
-	"github.com/leeola/fixity/indexes/snail"
-	"github.com/leeola/fixity/stores/disk"
+	"github.com/leeola/fixity/autoload"
 	"github.com/leeola/rubbish"
 	"github.com/leeola/rubbish/stores/whala"
 	homedir "github.com/mitchellh/go-homedir"
@@ -18,8 +15,7 @@ import (
 type Config struct {
 	DontExpandHome bool `toml:"dontExpandHome"`
 
-	// TODO(leeola): Change this to a fixity config autoload path / usage
-	FixityStorePath string `toml:"fixityStorePath"`
+	FixityConfigPath string `toml:"fixityConfigPath"`
 }
 
 // TODO(leeola): Change this to a fixity config autoload path / usage
@@ -27,11 +23,6 @@ func storeFromCtx(ctx *cli.Context) (rubbish.Store, error) {
 	configPath := ctx.GlobalString("config")
 	if configPath == "" {
 		return nil, errors.New("config PATH is required")
-	}
-
-	configPath, err := homedir.Expand(configPath)
-	if err != nil {
-		return nil, err
 	}
 
 	f, err := os.Open(configPath)
@@ -45,45 +36,25 @@ func storeFromCtx(ctx *cli.Context) (rubbish.Store, error) {
 		return nil, errors.Wrap(err, "failed to unmarshal config")
 	}
 
-	if conf.FixityStorePath == "" {
-		return nil, errors.New("missing required config value: FixityStorePath")
+	if conf.FixityConfigPath == "" {
+		return nil, errors.New("missing required config value: FixityConfigPath")
 	}
 
 	if !conf.DontExpandHome {
-		p, err := homedir.Expand(conf.FixityStorePath)
+		p, err := homedir.Expand(conf.FixityConfigPath)
 		if err != nil {
 			return nil, err
 		}
-		conf.FixityStorePath = p
+		conf.FixityConfigPath = p
 	}
 
-	sConf := disk.Config{
-		Path: filepath.Join(conf.FixityStorePath, "store"),
-	}
-	s, err := disk.New(sConf)
-	if err != nil {
-		return nil, err
-	}
-
-	iConf := snail.Config{
-		Path: filepath.Join(conf.FixityStorePath, "index"),
-	}
-	i, err := snail.New(iConf)
-	if err != nil {
-		return nil, err
-	}
-
-	kConf := local.Config{
-		Store: s,
-		Index: i,
-	}
-	k, err := local.New(kConf)
+	fixi, err := autoload.LoadFixity(conf.FixityConfigPath)
 	if err != nil {
 		return nil, err
 	}
 
 	wConf := whala.Config{
-		Fixity: k,
+		Fixity: fixi,
 	}
 	return whala.New(wConf)
 }
